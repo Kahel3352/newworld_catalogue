efe420dfc97644941a83c2e3c2e4d7c4c648e1e4
mlebeau <melanie33130@live.fr>
2018-03-30 09:55:16 +0200

    Modification du cahier des charges Creation du dossier ImgRessources avec ses fichiers Creation du dossier catalogueNewWorldMaquette

---
+++ catalogueNewWorldMaquette/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Catalogue NewWorld Maquette</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+<h1>Catalogue NewWorld Maquette</h1>
+</body>
+</html>
